0352c3f254ac31b25e4592346744e20e27bde89c
prios <prios@wtransnet.com>
2015-08-13 13:25:46 +0200

    downgrade de 22 a 21

---
+++ app/src/main/java/noox/jetkins/beans/User.java
@@ -0,0 +1,17 @@
+package noox.jetkins.beans;
+
+/**
+ * Datos del usuario
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/test/java/noox/jetkins/beans/UserTest.java
@@ -0,0 +1,28 @@
+package noox.jetkins.beans;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Test unitario de la clase User
+ */
+public class UserTest {
+
+    @Test
+    public void testUserName() {
+
+        String fakeName = "fakeName";
+
+        User user = new User();
+        user.setName(fakeName);
+
+        Assert.assertEquals(fakeName, user.getName());
+    }
+
+    @Test
+    public void failingTest() {
+        Assert.assertFalse(true);
+    }
+
+}
